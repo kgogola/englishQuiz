1f69dafaac9295a045cae1f160d54ba510574b04
kgogolaa <gogolinex@interia.pl>
2019-11-09 12:33:39 +0100

    Second commit

---
+++ src/com/company/Question.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Question {
+    String question;
+    String firstAnswer;
+    String secondAnswer;
+    String thirdAnswer;
+    String fourthAnswer;
+    String properAnswer;
+}
